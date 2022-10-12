70cf4f6f63325aca604c8359f75dc038ae98f563
Siddhartha Machi <siddhartha.machi@gmail.com>
2022-10-12 07:35:21 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -48,12 +48,18 @@ class Dev(Configuration):
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
+    
     'blog',
     'crispy_forms',
     'crispy_bootstrap5',
     'debug_toolbar',
     'blango_auth',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google'
     ]
 
     MIDDLEWARE = [
@@ -204,6 +210,11 @@ class Dev(Configuration):
     ACCOUNT_ACTIVATION_DAYS = 7
 
     REGISTRATION_OPEN = False
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 class Prod(Dev):
 
