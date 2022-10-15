f0725642314cfdd375cbc9b875a4b811c0dea22e
Siddhartha Machi <siddhartha.machi@gmail.com>
2022-10-15 06:36:33 +0000

    Finish viewsets and routers.

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList, PostDetail,UserDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 from rest_framework.authtoken import views
 from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+from rest_framework.routers import DefaultRouter
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -15,12 +16,18 @@ schema_view = get_schema_view(
     url=f"https://{os.environ.get('CODIO_HOSTNAME')}-8000.codio.io/api/v1/",
     public=True,
 )
+# Instantiating the router object and Registering the urls
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+# Same thing for the post objects
+router.register("posts", PostViewSet)
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
-    path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token),
+    # Urls are generated using the routers object...
+
+    # path("posts/", PostList.as_view(), name="api_post_list"),
+    # path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    
 
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 
@@ -29,7 +36,8 @@ urlpatterns = [
 urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns += [
-
+    path("auth/", include("rest_framework.urls")),
+    path("token-auth/", views.obtain_auth_token),
      re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
@@ -40,5 +48,7 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    # Includes both the post and tag model urls...
+    path("", include(router.urls)),
 
 ]
